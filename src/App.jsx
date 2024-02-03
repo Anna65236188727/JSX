c7f0d1fa66c8835c3e7e33113eb82ca1ede8edad
TEMAS629 <kirillov_art@list.ru>
2024-02-03 23:07:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,220 @@
+import React from 'react';
+
+function App() {
+  // Задание 7
+  // return <div>
+  // 	<p class='red'>Это абзац красного цвета</p>
+  //   <p class='underline'>Этот абзац подчеркивается</p>
+  // </div>;
+
+  // Задание 8
+  // return	<ul>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  // 	</ul>;
+
+  // Задание 9
+  // return (             //Добавлены ()
+  //   <div>
+  //     <p>text1</p>
+  //     <p>text2</p>
+  //     <p>text3</p>
+  //   </div>
+  // );
+
+
+  // Задание 10
+
+  // return(             //Добавлен пустой тег
+  // <>
+  //   <ul>
+  //     <li>text1</li>
+  //     <li>text2</li>
+  //     <li>text3</li>
+  //   </ul>
+  //   <ul>
+  //     <li>text1</li>
+  //     <li>text2</li>
+  //     <li>text3</li>
+  //   </ul>
+  // </>
+  // );
+
+  // return(             //Добавлен тег div
+  // <div>
+  //   <ul>
+  //     <li>text1</li>
+  //     <li>text2</li>
+  //     <li>text3</li>
+  //   </ul>
+  //   <ul>
+  //     <li>text1</li>
+  //     <li>text2</li>
+  //     <li>text3</li>
+  //   </ul>
+  // </div>
+  // );
+
+  // Задание 11
+  // return <input />;             //Добавлен /
+
+
+  // Задание 12
+  // return (
+  // <div>
+  //  <input />
+  //  <input />
+  //  <input />
+  // </div>
+  // );
+
+  // Задание 13
+  // const str1 = 'text1';
+  // const str2 = 'text2';
+
+  // return <div>
+  // 	<p>{str1}</p>
+  // 	<p>{str2}</p>
+  // </div>;
+
+  // Задание 14
+  // const name = 'user';
+  // const age  = '30';
+
+  // return <div>
+  // 	name: {name} <br/>
+  // 	age:  {age}
+  // </div>;
+
+  // Задание 15
+  // const arr = [1, 2, 3, 4, 5];
+
+  // return <ul>
+  //   <li>{arr[0]}</li>
+  //   <li>{arr[1]}</li>
+  //   <li>{arr[2]}</li>
+  //   <li>{arr[3]}</li>
+  //   <li>{arr[4]}</li>
+  // </ul>
+
+  // Задание 16
+  // const obj = {name: 'john', surname: 'smit'};
+
+  // return <div>
+  //   <p>name: {obj.name}</p>
+  //   <p>surname: {obj.surname}</p>
+  // </div>
+
+  // Задание 17
+  // const attr = 'block';
+
+  // return <div id={attr}>
+  // 	text
+  // </div>;
+
+  // Задание 18
+  // Пример
+  // const str = <p>text</p>;
+
+  // return <div>
+  //   {str}
+  // </div>;
+
+  // Задание 19
+  // const li1 = <li>text1</li>;
+	// const li2 = <li>text2</li>;
+	// const li3 = <li>text3</li>;
+
+  // return <main>
+	// 	{li1}
+  //   {li2}
+  //   {li3}
+	// </main>;
+
+  // Задание 20
+  // const items = <><li>text1</li><li>text2</li><li>text3</li></>;
+	
+	// return <ul>
+	// 	{items}
+	// </ul>
+
+  // Задание 21
+  // Пример
+  // const str = <main>
+	// 	text
+	// </main>;
+	
+	// return str;
+
+  // Задание 22
+  // return <div>
+	// 	<input /> <br />
+  //   <input /> <br />
+  //   <input />
+	// </div>;
+
+  // Задание 23.1
+  // return <div>
+	// 	<ul>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //     <li></li>
+  //   </ul>
+	// </div>;
+
+  // Задание 23.2
+  // return <div>
+	// 	<tbody>
+	// 		<tr>
+	// 			<td>1</td>
+	// 			<td>2</td>
+  //       <td>3</td>
+	// 		</tr>
+	// 		<tr>
+	// 			<td>4</td>
+	// 			<td>5</td>
+  //       <td>6</td>
+	// 		</tr>
+  //     <tr>
+	// 			<td>7</td>
+	// 			<td>8</td>
+  //       <td>9</td>
+	// 		</tr>
+	// 	</tbody>
+	// </div>;
+  
+  // Задание 24
+  
+		// 24.1
+    // result: 9
+
+    // 24.2
+    // result: john smit
+
+    // 24.3
+    // result: 2
+    
+
+  // Задание 25
+
+  // Задание 26
+
+  // Задание 27
+}
+
+export default App;
